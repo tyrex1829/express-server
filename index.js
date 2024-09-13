b3a7ce300f81b6dada9f14692f051f22ff3a6ea5
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-13 21:47:11 +0530

    basic server with 3 routes

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World, This is a landing page of our backend server");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Hello World, This is a about-us page of our backend server");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Hello World, This is a contact-us page of our backend server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port}`);
+});
