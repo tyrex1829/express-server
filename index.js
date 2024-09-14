3895f6844acf094266b2ede1305170e9a40aef22
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-14 13:33:50 +0530

    env variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middlewares. If wanna accept some data from the frontend side.
 // settings
