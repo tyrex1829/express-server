43a5e137beea022a8df04b97487f28d6ae8b1b2e
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-16 15:54:11 +0530

    add logger for info, warn, error

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +9,24 @@ const port = process.env.PORT || 3000;
 // middlewares. If wanna accept some data from the frontend side.
 // settings
 app.use(express.json()); // any data which comes in json format, we accept that.
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let storeData = [];
 let nextId = 1;
@@ -19,6 +39,7 @@ app.get("/", (req, res) => {
 
 // when u wanna save data in db, post data in db etc, use post
 app.post("/data", (req, res) => {
+  logger.warn("A post request is made to add new data.");
   const { name, price } = req.body; // to get body of data. (remember data inside body and in json format, when fetched)
   const newData = { id: nextId++, name, price };
   storeData.push(newData);
